faaf67c487beabc20c26767c1ecd614e9fecd2e1
Jaxible <jtk86@pitt.edu>
2024-10-22 18:15:10 -0400

    Update character_body_2d.gd
    gravity sort of works, jumping is weird

--- basic-frame/character_body_2d.gd
+++ basic-frame/character_body_2d.gd
@@ -4,15 +4,21 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	get_input()
+	# Handle jump.
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if input_direction:
+		velocity = input_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
