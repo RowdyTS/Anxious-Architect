95c30b8d20ef84438689a0366bd5ebc902fb3373
CriglerM02 <mfc34@pitt.edu>
2024-12-09 16:31:00 -0500

    bossroom music

--- Anxious Architect Final/Scripts/win_circle.gd
+++ Anxious Architect Final/Scripts/win_circle.gd
@@ -1,12 +1,14 @@
 extends Area2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
 
 
 func _ready():
-	
+	audio_stream_player_2d.play()
 	self.connect("body_entered", Callable(self, "_on_body_entered"))
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 
 func _on_body_entered(body):
+	audio_stream_player_2d.stop()
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	Dialogic.start("res://Assets/Reach Out.dtl")
 
